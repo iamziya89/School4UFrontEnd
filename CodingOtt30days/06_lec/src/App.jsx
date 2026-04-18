829d7ced3044903c6dd67855eea1af369af2e025
Mohd Ziya <iamziya53@gmail.com>
2026-04-18 05:52:26 +0530

    Frontend

---
+++ CodingOtt30days/06_lec/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+let App=()=>{
+  return(
+    <div className="bg-black text-white text-3xl">
+      <h2>Mohammad Ziya Shameem </h2>
+    </div>
+  )
+}
+export default App;
---
+++ CodingOtt30days/09_lec/src/App.jsx
@@ -0,0 +1,14 @@
+
+import 'animate.css';
+import {LogOut} from 'lucide-react';
+let App=()=>{
+  return(
+    <div>
+      <h1 className="text-9xl font-bold animate__animated animate__rubberBand animate__slower">React Js</h1>
+     <button>
+      <LogOut />
+      Mohd ZIya</button>
+    </div>
+  )
+}
+export default App;

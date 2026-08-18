833cf6967a4f6f7a6e39095c80ac7a47ce7a7e23
Mohd Ziya <iamziya53@gmail.com>
2026-08-19 03:04:05 +0530

    Data Structure Practice

---
+++ Data_Structure/Array_DataType/program1.js
@@ -0,0 +1,40 @@
+// 1. WAP to add an element at the end of an arrays
+
+// let arr =["MOHD", "MUSHEER", "ARBAZ"];
+// arr.push("ANAS ZIYA")
+// console.log(arr);
+
+
+// 2. WAP to add an element at the begining of an arrays
+// let arr =["MOHD", "MUSHEER", "ARBAZ"];
+// arr.unshift("ANAS ZIYA")
+// console.log(arr);
+
+
+// 3. WAP to remove an element at the last of an arrays
+// let arr =["MOHD", "MUSHEER", "ARBAZ"];
+// arr.pop();
+// console.log(arr);
+
+
+// 4. WAP to remove an element at the begining of an arrays
+// let arr =["MOHD", "MUSHEER", "ARBAZ"];
+// arr.shift();
+// console.log(arr);
+
+
+// 5. WAP to find the length of an array
+// let arr = [13, 25, 45, 65, "MOHD"]
+// console.log(arr.length);
+
+
+
+// 6. WAP to loop through all the elements of an arrays
+let arr=[21, 43, 22, 76, 109, 120]
+for( let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
+
+
+
+

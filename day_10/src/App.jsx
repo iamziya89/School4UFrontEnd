e90eb849ad66e14453191fa699f7a61655b9c0b0
Mohd Ziya <iamziya53@gmail.com>
2026-08-18 15:29:07 +0530

    Practice react router dom

--- day_10/src/App.jsx
+++ day_10/src/App.jsx
@@ -1,13 +1,20 @@
+import React from 'react'
 import { useState } from 'react'
-import './App.css'
 
-function App() {
+const App = () => {
+  let [show, setShow] =useState("Mohd");
 
+  function test(){
+    setShow(!show)
+  }
+  
   return (
     <div>
-      
+      <h1>Mohd Ziya Shameem</h1>
+      <button onClick={()=>{alert("MOHD ZIYA SHAMEEM")}}>Click Me!</button>
     </div>
   )
 }
 
 export default App
+

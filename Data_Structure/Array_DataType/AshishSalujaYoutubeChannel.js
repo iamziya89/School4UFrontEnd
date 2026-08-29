1ef8af760e7b0cb6d7f4e6ebb401fd12184ca97d
Mohd Ziya <iamziya53@gmail.com>
2026-08-29 17:43:44 +0530

    Practice

---
+++ Data_Structure/Array_DataType/AshishSalujaYoutubeChannel.js
@@ -0,0 +1,40 @@
+// console.log("MOHD ZIYA SHAMEEM");
+
+//  ARRAY
+// const arr=new Array(5).fill("ZIYA");
+// console.log(arr);
+
+// const arrof =Array.of("ZIYA", "SHAMEEM");
+// console.log(arrof);
+
+// const arrfrom=Array.from("SHAMEEM AHMAD")
+
+// console.log(arrfrom);
+
+// let arr =[2,2,2,4,4]
+// let str=""
+// for(let elm of arr){
+//     str+=elm
+// }
+// console.log(typeof str);
+
+// var plusOne = function(digits) {
+//     let str="";
+//     let arr=[]
+//     for(let elm of digits){
+//         str+=elm
+//     }
+//     let numStr=parseInt(str)+1;
+    
+// };
+
+// console.log(plusOne([1,2,3]))
+
+
+
+// OOPs Concept
+
+
+
+
+
--- crudApp/src/App.jsx
+++ crudApp/src/App.jsx
@@ -1,11 +1,79 @@
-import { useRef } from "react";
+// import { useRef } from "react";
+
+import { useState } from "react";
 
 
 const App = () =>{
+  const [userData, setUserData]=useState([])
+  function fetchData(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>{
+      return response.json();
+    }).then((user)=>{
+      console.log(user)
+      setUserData(user);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
 
   return(
-    <div>
-      
+    <div style={{
+      width   : '70%',
+      margin: '0 auto'
+    }}>
+      <h2 style={{
+        textAlign : 'center',
+      }}>CodingOtt</h2>
+      <button style={{
+        border : 'none',
+        padding: '8px 15px',
+        borderRadius: 6,
+        cursor: 'pointer',
+        fontSize:15,
+        fontWeight:'bold',
+        background:'blue',
+        color:'white'
+      }}
+      onClick={fetchData}>Fetch Data</button>
+      {
+        userData.map((items, index)=>{
+          return(
+            <div key={index} style={{
+        marginTop:'1rem',
+        border :'1px solid #ddd',
+        padding: '1.0rem 0',
+        boxShadow: '0 0 10px #ddd',
+        borderRadius:5,
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        background: '#abcffc'
+      }}>
+        <div style={{
+          padding: '0.5rem 0.8rem',
+          width:'50%',
+          height:'50%',
+          display:'flex',
+          flexDirection:'column',
+          justifyContent:'center',
+          alignItems:'center',
+          gap:'1rem',
+          background:'#b04520',
+          color:'White',
+          borderRadius:'0.5rem',
+          boxShadow: '10 20 30px #ddd',
+        }}>
+          <h3>Name : {items.name}</h3>
+          <h4>Email  : {items.email} </h4>
+          <h5>Phone No : {items.phone} </h5>
+        </div>
+      </div>
+          )
+        })
+      }
     </div>
   )
 }
